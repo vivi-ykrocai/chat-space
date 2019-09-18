81ab7cf618fa8460b638d4a376f7a8eb81a1644d
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-09-18 11:48:52 +0900

    インクリメンタルサーチ実装(修正)
    .limit(20)の消去

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id:current_user).limit(20)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id:current_user)
     respond_to do |format|
       format.html
       format.json
