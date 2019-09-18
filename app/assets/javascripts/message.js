53ff17540c22fe52ac42d559ad335f481633bf5c
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-09-18 20:35:30 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     var image_url = message.image?  `<img class="lower-message__image" src="${message.image}">`: '' ;
-    var html = `<div class="message">
+    var html = `<div class="message" data-id="${message.id}">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                     ${message.name}
@@ -20,6 +20,7 @@ $(function(){
                 </div>`
     return html;
   }
+  
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -42,4 +43,36 @@ $(function(){
       alert('error');
     })
   })
-})
+
+  var reloadMessages = function() {
+    var page_url = location.href;
+    if (page_url.match(/groups\/\d\/messages/)) {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.message:last').data('id');
+      
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: './api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log(messages)
+        var insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        });
+        console.log('success');
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+  };
+  setInterval(reloadMessages, 5000);
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@ json.body  @message.body
 json.image  @message.image.url
 json.name  @message.user.name
 json.created_at  @message.created_at.to_s
+json.id @message.id
